d942b1c7b3cce719c5a67d53b497a5aca7f03950
Julien <blondeau.ju@gmail.com>
2013-05-06 17:08:00 +0200

    subs can be downloaded

--- pyOpenSubDownloader.py
+++ pyOpenSubDownloader.py
@@ -5,10 +5,17 @@
 
 
 from xmlrpclib import ServerProxy
-from conf import Configuration
 import os
 import mimetypes
 import argparse
+import urllib2
+import tempfile
+import gzip
+
+
+class Configuration(object):
+    OPEN_SUBTITLES_URL = "http://api.opensubtitles.org/xml-rpc"
+    OPEN_SUBTITLES_USER_AGENT = "OS Test User Agent"
 
 
 class OpenSubDownloader():
@@ -68,8 +75,32 @@ class VideoFiles():
         else:
             print "wtf iz " + args.path + "?"
 
-    def hasSubtitles(self, videoPath):
-        os.path.splitext(filename)[0]
+
+def hasSubtitles(videoPath):
+    return os.path.isfile(os.path.splitext(videoPath)[0] + ".srt")
+
+
+def downloadUrlFile(url, path, name):
+    u = urllib2.urlopen(url)
+    tempFile = tempfile.mkstemp()[1]
+    f = open(tempFile, 'wb')
+
+    file_size_dl = 0
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+        file_size_dl += len(buffer)
+        f.write(buffer)
+    f.close()
+    f = gzip.open(tempFile, 'rb')
+    file_content = f.read()
+    f.close()
+    f = open(path + os.sep + name + ".srt", 'wb')
+    f.write(file_content)
+    f.close()
+    print "Subtitles downloaded for %s" % (name)
 
 
 if __name__ == "__main__":
@@ -78,24 +109,30 @@ if __name__ == "__main__":
     parser.add_argument('path', help='path of the video file or folder containing videos')
     parser.add_argument('--hash', help='using hash instead of filename', action='store_const', const=True)
     parser.add_argument('--force', help='replace existing subtitles', action='store_const', const=True)
+    parser.add_argument('-l', '--language', help='subtitles language code like eng or fre', nargs="?", const="fre")
     args = parser.parse_args()
 
     mimetypes.init()
     osd = OpenSubDownloader()
     osd.login()
 
+    if args.language is None:
+        #default french
+        args.language = 'fre'
+
     vf = VideoFiles(args.path)
 
     for videoFile in vf.filenames:
-        print videoFile
         if args.hash:
             raise NotImplementedError
         else:
-            osresult = osd.find({'sublanguageid': 'fre', 'query': videoFile})
+            osresult = osd.find({'sublanguageid': args.language, 'query': videoFile})
             if osresult is not None and osresult is not False:
-                for subEntry in osresult:
-                    print subEntry["SubDownloadLink"]
+                # for subEntry in osresult:
+                #     print subEntry["SubDownloadLink"]
+                #get first one
+                if not hasSubtitles(vf.path + os.sep + videoFile) or args.force:
+                    subUrl = osresult[0]["SubDownloadLink"]
+                    downloadUrlFile(subUrl, vf.path, os.path.splitext(videoFile)[0])
             else:
                 print "no sub found for " + videoFile
-    print vf.path
-
